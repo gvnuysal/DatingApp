258754c17d14bb3e070051611fc26fc14787ce00
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-01 11:48:16 +0300

    Add Extensions method

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,9 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -24,28 +18,14 @@ namespace API
           // This method gets called by the runtime. Use this method to add services to the container.
           public void ConfigureServices(IServiceCollection services)
           {
-               services.AddTransient<ITokenService, TokenService>();
-               services.AddDbContext<DataContext>(options =>
-               {
-                    options.UseSqlite(_config.GetConnectionString("DefaultConnection"));//sql lite connection strings
-               });
+              services.AddApplicationServices(_config);
                services.AddControllers();
                services.AddSwaggerGen(c =>
                {
                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
                });
                services.AddCors();
-               services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                       .AddJwtBearer(options =>
-                       {
-                            options.TokenValidationParameters = new TokenValidationParameters
-                            {
-                                 ValidateIssuerSigningKey = true,
-                                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                                 ValidateIssuer = false,
-                                 ValidateAudience = false
-                            };
-                       });
+              services.AddIdentityServices(_config);
           }
 
           // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
