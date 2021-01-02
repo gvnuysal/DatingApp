b677fd3f6500d26ffe9dac0a899dd645db28b5b1
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-03 00:34:44 +0300

    added toast service for notification

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import{ HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+
 
 @NgModule({
   declarations: [
@@ -32,6 +34,9 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right'
+    }),
     BsDropdownModule.forRoot()
   ],
   providers: [],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -7,21 +8,27 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  @Output() cancelRegister=new EventEmitter()
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService:AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   register() {
-    this.accountService.register(this.model).subscribe(response=>{
-      this.cancel();
-    },error=>{
-      console.log(error)
-    })
+    this.accountService.register(this.model).subscribe(
+      (response) => {
+        this.cancel();
+      },
+      (error) => {
+        this.toastr.error(error.error);
+      }
+    );
   }
   cancel() {
-     this.cancelRegister.emit(false);
+    this.cancelRegister.emit(false);
   }
 }
