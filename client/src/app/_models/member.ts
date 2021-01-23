d94dad68db335650c33489eba41dd56a897a4cf3
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-23 23:00:14 +0300

    add member and photo inteface for client

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,23 @@
+import { Photo } from "./photo";
+
+export interface Member {
+  id: number;
+  userName: string;
+  photoUrl: string;
+  age: number;
+  passwordHash: string;
+  passwordSalt: string;
+  dateOfBirth: string;
+  knownAs: string;
+  created: Date;
+  lastActive: Date;
+  gender: string;
+  introduction: string;
+  lookingFor: string;
+  interests: string;
+  city: string;
+  country: string;
+  photos: Photo[];
+}
+
+
