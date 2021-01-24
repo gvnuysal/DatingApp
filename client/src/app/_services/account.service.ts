46f5db4e45827e12253bd772d260e3b210de6fce
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-24 11:01:21 +0300

    create member card

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,16 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  baseUrl: string = 'https://localhost:44325/api/';
+  baseUrl: string = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+
+  }
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,30 @@
+import {
+  HttpClient,
+  HttpHeaders,
+  JsonpClientBackend,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MembersService {
+  baseurl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMembers(){
+    return this.http.get<Array<Member>>(this.baseurl + 'users', httpOptions);
+  }
+  getMember(username){
+    return this.http.get<Member>(this.baseurl+'user/'+username,httpOptions);
+  }
+}
