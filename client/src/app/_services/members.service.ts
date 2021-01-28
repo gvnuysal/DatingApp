5c69503a770f44fe6f2f6baa4756e53568468d28
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-28 18:11:54 +0300

    added store

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -8,16 +10,32 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseurl = environment.apiUrl;
+  members:Array<Member>=[];
 
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Array<Member>>(this.baseurl + 'users');
+    if(this.members.length>0) return of(this.members);
+    return this.http.get<Array<Member>>(this.baseurl + 'users').pipe(
+      map(members=>{
+          this.members=members;
+          return members;
+      })
+    );
   }
   getMember(username) {
+    const member=this.members.find(x=>x.userName===username);
+    if(member!==undefined){
+      return of(member);
+    }
     return this.http.get<Member>(this.baseurl + 'users/' + username);
   }
   updateMember(member: Member) {
-    return this.http.put(this.baseurl + 'users', member);
+    return this.http.put(this.baseurl + 'users', member).pipe(
+      map(()=>{
+        const index=this.members.indexOf(member);
+        this.members[index]=member;
+      })
+    );
   }
 }
